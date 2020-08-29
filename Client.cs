9b02f387bc6b1669a16f5f67ed10821a71fee5bd
gregory_vovchok <gregory.vovchok@gmail.com>
2020-08-28 23:27:58 -0700

    Initial

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+
+namespace ChessClient
+{
+    public class Client
+    {
+        public string host { get; private set; }
+        public string user { get; private set; }
+
+        public Client(string host, string user)
+        {
+            this.host = host;
+            this.user = user;
+        }
+
+        string CallServer(string param = "")
+        {
+            WebRequest request = WebRequest.Create(host + "/" + param);
+            WebResponse response = request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+                return reader.ReadToEnd();
+        }
+
+        public void GetCurrentGame()
+        {
+            Console.WriteLine(CallServer("Games"));
+        }
+    }
+}
